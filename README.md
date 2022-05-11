6f7e6825e8543131872fbefab484c8e2d93d8081
Shubham Prajapat <shubhamp.developer@gmail.com>
2022-05-11 17:53:06 +0530

    Added ReactJS
    With typescript template. Personalized the logo, favicon and index.html

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # discover-trending-repositories
+
 A web app for discovering trending repositories on GitHub.
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+	return <div className="app">silence is golden</div>;
+}
+
+export default App;
